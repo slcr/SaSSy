782bade7361ee0843edc2d0b80eefc69768e89f9
Chris Jahn <soulcore@gmail.com>
2012-08-02 10:53:14 +0200

    added line-comments: true for better debugging in firebug.

--- config.rb
+++ config.rb
@@ -30,7 +30,7 @@ output_style = :compact
 environment = :production
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+line_comments = true
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
