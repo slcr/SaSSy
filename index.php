48b0bb72a3f46ea73afcb840e903f552ab43e980
Chris Jahn <soulcore@gmail.com>
2013-01-03 11:38:41 +0100

    cleanup
    cleaned some parts in _typography.scss, _base.scss, _variable.scss and
    added a html5 page structure in index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<title>TITLE</title>
+ 	
+	<meta name="author" content="Chris Jahn <c.jahn@oktober.de>" />
+	<meta name="publisher" content="http://www.oktober.de" />
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<!--[if IE]><![endif]-->
+	<link media="all" rel="stylesheet" type="text/css" href="style.css">
+
+	<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+	<script type="text/javascript">google.load("jquery", "1");</script>
+	<script type="text/javascript" src="/js/functions.js"></script>	
+	<!--[if lt IE 9]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+</head>
+<body>
+
+	<div id="wrapper">
+		
+	</div>
+
+<script>
+  var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+  (function(d, t) {
+    var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.src = '//www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s);
+  }(document, 'script'));
+</script>
+
+
+</body>
+</html>
